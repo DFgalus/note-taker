b20ba2fb837978cee2f164c0203163daf99c3df1
Dgalus <damiangalus7@gmail.com>
2023-02-15 00:10:34 -0500

    Created server.js for server code

---
+++ Develop/public/server.js
@@ -0,0 +1,10 @@
+
+//import dependencies
+const express = require('express');
+const path = require('path');
+
+//add database to store notes
+const dbData = require('../db/db.json');
+
+
+const app = express();
